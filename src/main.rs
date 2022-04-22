e45021578c59a929fb894dafee5cb80db216b44b
Nats <natsishere@gmail.com>
2022-04-23 00:27:36 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,30 @@ fn main() {
     io::stdin().read_line(&mut id).expect("Impossible de lire la ligne");
     id.pop();
     if id.contains("\r") { id.pop(); }
-    share(id);
+    δ(id);
 }
-fn share(id: String) {
-    const LIMIT_THREAD: i32 = 200;
+fn δ(id: String) {
+    const LIMIT_THREAD: i32 = 5000;
     let count = Arc::new(AtomicUsize::new(0));
     for _ in 0..LIMIT_THREAD {
         let clone = id.clone();
         let clone_arc = count.clone();
         thread::spawn(move || loop {
-            let client = reqwest::blocking::Client::new();
-            let r = client.post("http://api19.tiktokv.com/aweme/v1/aweme/stats/?channel=tiktok_web&device_type=SM-G9900&device_id=9999999999999999999&os_version=11&version_code=220400&app_name=tiktok_web&device_platform=android&aid=1988")
-                .header(CONTENT_TYPE, "application/x-www-form-urlencoded; charset=UTF-8")
-                .query(&[("item_id", &clone), ("share_delta", &"1".to_string())])
-                .send();
+             let client = reqwest::blocking::Client::new();
+             let r = client.post("http://api19.tiktokv.com/aweme/v1/aweme/stats/?channel=tiktok_web&device_type=SM-G9900&device_id=9999999999999999999&os_version=11&version_code=220400&app_name=tiktok_web&device_platform=android&aid=1988")
+                 .header(CONTENT_TYPE, "application/x-www-form-urlencoded; charset=UTF-8")
+                 .query(&[("item_id", &clone), ("share_delta", &"1".to_string())])
+                 .send();
             if r.is_ok() {
-                clone_arc.fetch_add(1, Ordering::Relaxed);
-            }
+                 clone_arc.fetch_add(1, Ordering::Relaxed);
+           }
         });
     }
     let now = Instant::now();
     thread::sleep(Duration::from_secs(1));
     loop {
         let time = now.elapsed().as_secs() as usize;
-        winconsole::console::set_title(format!("Vitesse: {}/s, Time elapsed: {}s", (count.load(Ordering::Relaxed) / time), time).trim()).unwrap();
+        winconsole::console::set_title(format!("Total: {} - Per second: {} - Time: {}", count.load(Ordering::Relaxed),(count.load(Ordering::Relaxed) / time), time).trim()).unwrap();
     }
 }
+
